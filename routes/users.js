e198b1f600a05d89e3f0c37354655885f1389875
omar <omar.elsawy@yahoo.com>
2019-01-09 16:33:09 +0200

    'm'

---
+++ routes/users.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const config = require('../config/database');
+const jwt = require('jsonwebtoken');
+
+//register
+router.post('/register' , (req , res , next)=>{
+   let newUser = new User({
+   	  name: req.body.name,
+   	  email: req.body.email,
+   	  username: req.body.username,
+   	  password: req.body.password
+   });
+
+   User.addUser(newUser , (err , user) => {
+     if (err) {
+     	res.json({success: false , msg: 'failed to register'});
+     }else{
+     	res.json({success: true , msg: 'user registerd'});
+     }
+   });
+});
+
+//authenticate
+router.post('/authenticate' , (req , res , next)=>{
+   const username = req.body.username;
+   const password = req.body.password;
+
+   User.getUserByUsername(username , (err , user) => {
+      if (err) throw err;
+      if (!user) {
+      	  return res.json({
+          success: false,
+          msg: 'user not found'
+      	});
+      }
+      User.comparePassword(password , user.password , (err , isMatch) => {
+         if (err) throw err;
+         if (isMatch) {
+         	const token = jwt.sign(user.toJSON() , config.secret , {
+         		expiresIn: 604800 //1 week
+         	});
+         	res.json({
+         		success: true,
+         		token: "JWT "+token,
+         		user: {
+         			id: user._id,
+         			name: user.name,
+         			username: user.username,
+         			email: user.email
+         		}
+         	});
+         }
+         else{
+         	  res.json({
+              success: false,
+              msg: 'wrong password'
+      	});
+         } 
+      });
+   });
+});
+
+//profile
+router.get('/profile' , passport.authenticate('jwt' , {session: false}) , (req , res , next)=>{
+   res.json({user: req.user});
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
